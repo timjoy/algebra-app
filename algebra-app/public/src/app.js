db93ae7eaab7744832c3e66ab07be08fd6038db3
Timothy Joy <tim.joy47@gmail.com>
2019-01-01 21:36:55 -0600

    basic React app setupand rendered a couple lines of text

---
+++ algebra-app/public/src/app.js
@@ -0,0 +1,14 @@
+class AlgebraApp extends React.Component {
+ 
+  
+  render(){
+    return (
+      <h1>Algebra App</h1>
+    );
+  };
+};
+
+
+
+ReactDOM.render(<AlgebraApp />, document.getElementById('app'));
+
