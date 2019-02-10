7fa93b6d983738fae6438a37c5f3c04355005f33
Timothy Joy <tim.joy47@gmail.com>
2019-02-10 08:46:47 -0600

    made changes to Fraction

--- algebra-app/public/scripts/app.js
+++ algebra-app/public/scripts/app.js
@@ -1,6 +1,8 @@
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -18,57 +20,69 @@ var AlgebraApp = function (_React$Component) {
 
     _this.handleSubmit = _this.handleSubmit.bind(_this);
     _this.handleChange = _this.handleChange.bind(_this);
-    _this.state = { value: props.value };
+    // this.state = {value: props.value};
+    _this.state = {
+      wholeNumber: props.wholeNumber,
+      fraction: props.fraction
+    };
     return _this;
   }
 
   _createClass(AlgebraApp, [{
-    key: "handleSubmit",
+    key: 'handleSubmit',
+
 
     //write the habndleSubmit function w/ a setState inside it :
-    value: function handleSubmit(event) {
-      var _this2 = this;
 
-      alert('You just submitted the fraction' + this.state.value);
+
+    value: function handleSubmit() {
+
+      alert('You just submitted the fraction ' + this.state.fraction);
       e.preventDefault();
-      this.setState(function (prevState) {
+      console.log('submitted');
+
+      this.setState(function () {
         return {
-          value: _this2.state.value
+          fraction: 1
+          // fraction: this.state.fraction,
+          // wholeNumber: this.state.wholeNumber
         };
       });
     }
   }, {
-    key: "handleChange",
-    value: function handleChange(event) {
-      this.setState({ value: event.target.value });
+    key: 'handleChange',
+    value: function handleChange() {
+      this.setState({ wholeNumber: this.state.wholeNumber,
+        fraction: this.state.fraction });
+      // alert ('handleChange is working');
+      console.log(this.state.fraction);
+      // this.setState({value: event.target.value});
+      // this.setState({value: state.name.value});
+
+      console.log('typing!!');
     }
   }, {
-    key: "render",
+    key: 'render',
     value: function render() {
+      var _React$createElement;
+
       return React.createElement(
-        "div",
+        'div',
         null,
         React.createElement(
-          "h1",
+          'h1',
           null,
-          "Algebra App"
+          'Algebra App'
         ),
         React.createElement(
-          "h2",
+          'h2',
           null,
-          "Type In A Fraction =>"
+          'Type In A Fraction =>'
         ),
-        React.createElement(
-          "form",
-          { onSubmit: this.handleSubmit },
-          React.createElement(
-            "label",
-            null,
-            "Fraction:",
-            React.createElement("input", { type: "text", onChange: this.handleChange, value: this.state.value })
-          ),
-          React.createElement("input", { type: "submit", value: "Submit" })
-        )
+        React.createElement(Fraction, (_React$createElement = {
+          fraction: this.state.fraction,
+          wholeNumber: this.state.wholeNumber
+        }, _defineProperty(_React$createElement, 'wholeNumber', this.state.wholeNumber), _defineProperty(_React$createElement, 'handleChange', this.handleChange), _defineProperty(_React$createElement, 'handleSubmit', this.handleSubmit), _React$createElement))
       );
     }
   }]);
@@ -77,21 +91,43 @@ var AlgebraApp = function (_React$Component) {
 }(React.Component);
 
 ;
+// const Action = (props) => { 
+//change Fraction to a class
+var Fraction = function Fraction(props) {
 
-AlgebraApp.defaultProps = {
-  value: ''
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'form',
+      { onSubmit: props.handleSubmit },
+      React.createElement(
+        'label',
+        null,
+        'Whole Number:',
+        React.createElement('input', {
+          type: 'number',
+          name: 'wholeNumber',
+          onChange: props.handleChange,
+          value: props.wholeNumber
+        })
+      ),
+      React.createElement('br', null),
+      React.createElement(
+        'label',
+        null,
+        'Fraction:',
+        React.createElement('input', {
+          type: 'number',
+          name: 'fraction',
+          onChange: props.handleChange,
+          value: props.fraction
+        })
+      ),
+      React.createElement('br', null),
+      React.createElement('input', { type: 'submit', name: 'Submit' })
+    )
+  );
 };
 
 ReactDOM.render(React.createElement(AlgebraApp, null), document.getElementById('app'));
-
-/* handleAddOption(option) {
-  if (!option) {
-    return 'Enter valid value to add item';
-  } else if (this.state.options.indexOf(option) > -1) {
-    return 'This option already exists';
-  }
-
-  this.setState((prevState) => ({
-    options: prevState.options.concat(option)
-  }));
-} */
--- algebra-app/public/src/app.js
+++ algebra-app/public/src/app.js
@@ -3,54 +3,91 @@ class AlgebraApp extends React.Component {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this); 
-    this.state = {value: props.value};
+    // this.state = {value: props.value};
+    this.state = {
+      wholeNumber: props.wholeNumber,
+      fraction: props.fraction
+    };
   };
+  
   //write the habndleSubmit function w/ a setState inside it :
-  handleSubmit(event) {
-    alert ('You just submitted the fraction'+ this.state.value);
+  
+  
+  handleSubmit() {
+    
+    alert ('You just submitted the fraction '+ this.state.fraction);
     e.preventDefault();
-    this.setState(prevState => ({
-      value: this.state.value
+    console.log('submitted')
+    
+    this.setState(() => ({ 
+      fraction: 1
+      // fraction: this.state.fraction,
+      // wholeNumber: this.state.wholeNumber
     }));
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value})
   };
-
+    
+  handleChange() {
+    this.setState({wholeNumber: this.state.wholeNumber,
+      fraction: this.state.fraction});
+    // alert ('handleChange is working');
+    console.log(this.state.fraction)
+    // this.setState({value: event.target.value});
+    // this.setState({value: state.name.value});
+    
+    console.log('typing!!')
+  }
+  
   render(){
     return (
       <div>
         <h1>Algebra App</h1>
           <h2>Type In A Fraction =></h2>
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Fraction:
-              <input type="text" onChange={this.handleChange} value={this.state.value} />
-            </label>
-            <input type="submit" value="Submit" />
-          </form>
+        <Fraction
+          fraction={this.state.fraction}
+          wholeNumber={this.state.wholeNumber}
+          wholeNumber={this.state.wholeNumber}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
       </div>
     );
   };
+};  
+// const Action = (props) => { 
+//change Fraction to a class
+const Fraction = (props) => {
+  
+    return  (
+        <div>  
+          <form onSubmit={props.handleSubmit}>
+            <label>
+              Whole Number:
+              <input 
+                type="number" 
+                name="wholeNumber"
+                onChange={props.handleChange} 
+                value={props.wholeNumber} 
+              />
+            </label>
+            <br />
+            <label> 
+               Fraction:
+               <input 
+                type="number" 
+                name="fraction"
+                onChange={props.handleChange} 
+                value={props.fraction} 
+               />
+            </label>
+            <br />
+            <input type="submit" name="Submit" />
+          </form>
+        </div>
+    );
 };
 
-AlgebraApp.defaultProps = {
-  value: ''
-};
-
+      
 ReactDOM.render(<AlgebraApp />, document.getElementById('app'));
 
 
 
-/* handleAddOption(option) {
-  if (!option) {
-    return 'Enter valid value to add item';
-  } else if (this.state.options.indexOf(option) > -1) {
-    return 'This option already exists';
-  }
-
-  this.setState((prevState) => ({
-    options: prevState.options.concat(option)
-  }));
-} */
